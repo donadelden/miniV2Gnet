42196d261375474f6b5c7f5e02ea70c222ad0cfd
Denis <denisdonadel@gmail.com>
2020-12-23 19:04:20 +0100

    Now it is possible to spawn SE and EV in xterm. For SE it is mandatory. Some other fixes.

--- mininet/easyScratch.py
+++ mininet/easyScratch.py
@@ -12,6 +12,7 @@
 from mininet.net import Mininet
 from v2g import EV, SE
 from mininet.cli import CLI
+from time import sleep
 
 net = Mininet()
 
@@ -19,8 +20,12 @@ se1 = net.addHost("se1", SE)
 ev1 = net.addHost("ev1", EV)
 
 net.addLink(se1, ev1)
-#se1.start(background=True)
 
-CLI( net )
+sleep(1)  # IMPORTANT! Give a second to the net to complete the setup (otherwise crashes are possible)
+net.terms += [se1.startCharge()]  # append to net.terms to enable "exit" command to close it
+sleep(1)
+net.terms += [ev1.charge(in_xterm=True)]
+
+CLI(net)
 
 net.stop()
--- mininet/v2g.py
+++ mininet/v2g.py
@@ -2,19 +2,17 @@
 Additional Nodes for Vehicle to Grid (V2G) communication
 """
 
-from os import popen
-from subprocess import Popen
-import signal
-import shlex
-from mininet.node import Node
-from mininet.moduledeps import pathCheck
-from mininet.link import Intf
-import sys
-import fileinput
 import atexit  # clean the mess on exit
+import fileinput
 # to generate the random prefix for folders
-import random, string
-from mininet.net import Mininet
+import random
+import string
+import sys
+from os import popen
+
+from mininet.term import makeTerm
+from mininet.moduledeps import pathCheck
+from mininet.node import Node
 
 
 class Electric(Node):
@@ -32,25 +30,31 @@ class Electric(Node):
             self.RISE_PATH = "/usr/share/.miniV2G/RiseV2G"
 
         # check if it exists
-        if "No such file or directory" in popen('ls ' + self.RISE_PATH).read():
+        if "No such file or directory" in popen('ls {}'.format(self.RISE_PATH)).read():
             exit(
                 "*** Fatal error: directory %s not found. Select the right folder which contains the needed jar files.")
         # TODO: check for the specific jar files
 
+        # initialize the subprocess object
+        self.proc = None
+
         Node.__init__(self, name, **kwargs)
 
         # setup a random prefix for folders (to better cleanup everything in the end)
-        self.FOLDER_PREFIX = ''.join(random.choice(string.ascii_lowercase) for i in range(4))
+        prefix_len = 4
+        self.FOLDER_PREFIX = ''.join(random.choice(string.ascii_lowercase) for i in range(prefix_len))
 
         # cleanup of the generated folder
         def cleaner():
             print('*** Cleaning up the mess')
-            popen("rm -rd " + self.FOLDER_PREFIX + "*")
+            popen("rm -rd {}*".format(self.FOLDER_PREFIX))
 
         atexit.register(cleaner)
 
     def intfSetup(self, folder, intfName=None):
-        """Set the intfName on the .properties file"""
+        """Sets the intfName on the .properties file.
+        :param folder: the subfolder containing the designated RiseV2G jar file
+        :param intfName: """
         # to decide if it is an ev or se it check the path;
         # if you change the default folder it is better to change this stuff
         if "ev" in folder:
@@ -60,7 +64,7 @@ class Electric(Node):
 
         for line in fileinput.input([folder + "/" + prefix + "Config.properties"], inplace=True):
             if line.strip().startswith('network.interface'):
-                line = 'network.interface = ' + intfName + '\n'
+                line = 'network.interface = {}\n'.format(intfName)
             sys.stdout.write(line)
 
 
@@ -73,80 +77,97 @@ class EV(Electric):
         self.name = str(name)
         Electric.__init__(self, self.name, path, **kwargs)
 
-        self.folder = self.FOLDER_PREFIX + "_ev_" + self.name
-        self.cmd("mkdir " + self.folder)
-        self.cmd("cp " + self.RISE_PATH + "/EVCCConfig.properties " + self.folder + "/")
-        # this cp can resist to update of risev2g but you must have only one version
-        self.cmd("cp " + self.RISE_PATH + "/rise-v2g-evcc-*.jar " + self.folder + "/")
+        self.folder = "{}_ev_{}".format(self.FOLDER_PREFIX, self.name)
+        self.cmd("mkdir {}".format(self.folder))
+        self.cmd("cp {}/EVCCConfig.properties {}/".format(self.RISE_PATH, self.folder))
+        # this cp can resist to update of risev2g but you must have onyl one version
+        self.cmd("cp {}/rise-v2g-evcc-*.jar {}/".format(self.RISE_PATH, self.folder))
         # cd into the right folder
-        self.cmd("cd ./" + self.folder)
+        self.cmd("cd ./{}".format(self.folder))
 
-    def charge(self, background=False, intf=None):
-        """Starting the charging process"""
+    def charge(self, in_xterm=False, intf=None):
+        """Starting the charging process.
+        :param in_xterm: True to run the charge inside an xterm instance. Default: False.
+        :param intf: the interface to which search for EVSE. Default: None, use the default interface."""
+
         print("*** Looking for EVSE and start charging...")
-
         # setting the interface (default: default interface)
         if intf is None:
             intf = self.intf().name
         self.intfSetup(self.folder, intf)
 
-        p = self.popen("cd ./" + self.folder + "; java -jar rise-v2g-evcc-*.jar", shell=True)
-        if not background:
-            proc_stdout = p.communicate()[0].strip()
+        if in_xterm:
+            # run inside an xterm. You must append the return value to net.terms to terminal on exit.
+            command = "cd ./{}; java -jar rise-v2g-evcc-*.jar; bash -i".format(self.folder)
+            # this return a list of just one xterm, so [0] is needed
+            self.proc = makeTerm(self, cmd="bash -i -c '{}'".format(command))[0]
+            return self.proc
+        else:
+            self.proc = self.popen("cd ./{}; java -jar rise-v2g-evcc-*.jar".format(self.folder), shell=True)
+            # print the stdout to the CLI at the end of the charging process
+            proc_stdout = self.proc.communicate()[0].strip()
             print(proc_stdout)
 
 
 class SE(Electric):
     """An EV Supply Equipment (EVSE) is a Node containing which can
     provide charging services to an EV by communication using the
-    Supply Equipment Communication Controller (SECC) """
+    Supply Equipment Communication Controller (SECC)
+    """
 
     def __init__(self, name, path=None, **kwargs):
         self.name = str(name)
         Electric.__init__(self, self.name, path, **kwargs)
 
-        self.folder = self.FOLDER_PREFIX + "_se_" + self.name
-        self.cmd("mkdir " + self.folder)
-        self.cmd("cp " + self.RISE_PATH + "/SECCConfig.properties " + self.folder + "/")
+        self.folder = "{}_se_{}".format(self.FOLDER_PREFIX, self.name)
+        self.cmd("mkdir {}".format(self.folder))
+        self.cmd("cp {}/SECCConfig.properties {}/".format(self.RISE_PATH, self.folder))
         # this cp can resist to update of risev2g but you must have onyl one version
-        self.cmd("cp " + self.RISE_PATH + "/rise-v2g-secc-*.jar " + self.folder + "/")
+        self.cmd("cp {}/rise-v2g-secc-*.jar {}/".format(self.RISE_PATH, self.folder))
         # cd into the right folder
-        self.cmd("cd ./" + self.folder)
+        self.cmd("cd ./{}".format(self.folder))
 
-    def startCharge(self, background=False, intf=None):
-        """ Start the listening phase.
-            backgroud: True if you want to do other things from the CLI"""
+    def startCharge(self, intf=None):
+        """
+        Spawn an xterm and start the listening phase in it.
+        It is not possible to launch it without xterm because otherwise it sometime randomly crashes.
+        :param intf: Interface in which listen for charging requests. If None, default is used.
+        :returns A popen xterm instance. To be appended to "net.terms" to assure a correct close on exit."""
 
         # setting the interface (default: default interface)
         if intf is None:
             intf = self.intf().name
         self.intfSetup(self.folder, intf)
 
-        # better promising approach, not yet perfet (TODO)
         print("*** Starting waiting for EVs...")
-        self.p = self.popen("cd ./" + self.folder + "; java -jar rise-v2g-secc-*.jar", shell=True)
-        if not background:
-            proc_stdout = self.p.communicate()[0].strip()
-            print(proc_stdout)
+        command = "cd ./{}; java -jar rise-v2g-secc-*.jar; bash -i".format(self.folder)
+        # this return a list of just one xterm, so [0] is needed
+        self.proc = makeTerm(self, cmd="bash -i -c '{}'".format(command))[0]
+        return self.proc
 
     def state(self):
-        """ Print and return the state of a backgrounded process"""
-        if hasattr(self, 'p'):
-            state = self.p.poll()
+        """ Print and return the state of a process
+        :return the state of the process or False if the process does not exists."""
+        if self.proc is not None:
+            state = self.proc.poll()
             if state is None:
                 print("* Running!")
             else:
                 print("* Stopped (State: {}).".format(state))
             return state
         else:
-            print("* The process does not exist. Call .start() first.")
-            return None
+            print("* The process does not exist. Call .startCharge() first.")
+            return False
 
     def stopCharge(self):
-        # TODO: maybe can be usefull to print stdout
-        if hasattr(self, 'p'):
-            state = self.p.terminate()  # send_signal(signal.SIGINT)
+        """Stops the charging process.
+        :return True if stopped successfully, False if the process does not exists"""
+        # TODO: maybe can be useful to print stdout
+        if self.proc is not None:
+            self.proc.kill()
+            self.proc = None
             print("* Stopped successfully.")
+            return True
         else:
-            print("* The process does not exist. Call .start() first.")
-            return None
+            print("* The process does not exist. Call .startCharge() first.")
+            return False
