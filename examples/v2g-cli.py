af0378616de46d0285cc43d7015ccb54d9ac6c5b
Luca Attanasio <blackwiz4rd>
2021-01-08 11:28:21 +0100

    wireshark can capture v2g-cli

--- examples/v2g-cli.py
+++ examples/v2g-cli.py
@@ -10,23 +10,36 @@
 """
 
 from mininet.net import Mininet
-from v2g import EV, SE
+from mininet.node import Controller
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+
+from v2g import EV, SE # TODO: replace with mininet.v2g
 from time import sleep
 
 def v2gNet():
 
     "Create a network with an EV and SE connected via a link."
 
+    start_on_load = False
+
     net = Mininet(  )
 
-    info( '*** Adding SE and EV\n' )
-    se1 = net.addHost("se1", SE)
-    ev1 = net.addHost("ev1", EV)
+    net = Mininet( controller=Controller )
 
-    info( '*** Creating link\n' )
-    net.addLink( se1, ev1 )
+    info( '*** Adding controller\n' )
+    net.addController( 'c0' )
+
+    info( '*** Adding hosts: SE and EV\n' )
+    se1 = net.addHost("se1", SE, ip='10.0.0.1' )
+    ev1 = net.addHost("ev1", EV, ip='10.0.0.2' )
+
+    info( '*** Adding switch\n' )
+    s1 = net.addSwitch( 's1' )
+
+    info( '*** Creating links\n' )
+    net.addLink( se1, s1 )
+    net.addLink( ev1, s1 )
 
     sleep(1)  # IMPORTANT! Give a second to the net to complete the setup (otherwise crashes are possible)
 
@@ -38,13 +51,15 @@ def v2gNet():
     info( '     - With `py se1.startCharge(in_xterm=True)` the SE will wait for charging EVs.\n' )
     info( '     - With `py ev1.charge(in_xterm=True)` the EV will start charging in the linked SE.\n' )
 
-    info( '*** Starting charge on the SE.\n' )
-    # TODO: move the sleep command to the SE and EV charge.
-    sleep( 1 ) # IMPORTANT! Give a second to the net to complete the setup (otherwise crashes are possible)
-    net.terms += [ se1.startCharge() ]
-    info( '*** EV is charging.\n' )
-    sleep( 1 )
-    net.terms += [ ev1.charge( in_xterm=True ) ]
+    if start_on_load == True:
+        info( '*** Starting charge on the SE.\n' )
+        # TODO: move the sleep command to the SE and EV charge.
+        sleep( 1 ) # IMPORTANT! Give a second to the net to complete the setup (otherwise crashes are possible)
+        net.terms += [ se1.startCharge() ]
+        info( '*** EV is charging.\n' )
+        sleep( 1 )
+        net.terms += [ ev1.charge( in_xterm=True ) ]
+    
     CLI( net )
 
     info( '*** Stopping network' )
